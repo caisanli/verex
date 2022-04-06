b80fcf4fe6080462a9b198897c9300a210ca00b5
CaisanLi <caisanli_0105@163.com>
2022-04-06 20:40:26 +0800

    Initial Commit

---
+++ verex/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  verex
+//
+//  Created by 蔡三李 on 2022/4/6.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ verex/verexApp.swift
@@ -0,0 +1,17 @@
+//
+//  verexApp.swift
+//  verex
+//
+//  Created by 蔡三李 on 2022/4/6.
+//
+
+import SwiftUI
+
+@main
+struct verexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
