dabb0ddd8fd2aa4583f7c7c7fe55051d0134b5d8
CaisanLi <caisanli_0105@163.com>
2022-04-12 22:49:03 +0800

    feat：增加首页标签页组件

---
+++ verex/Components/HomeNodeTabs.swift
@@ -0,0 +1,55 @@
+//
+//  HomeNodeTabs.swift
+//  verex
+//
+//  Created by 蔡三李 on 2022/4/10.
+//
+
+import SwiftUI
+
+
+/// 首页节点标签页
+struct HomeNodeTabs: View {
+    // 当前选中 tab
+    @Binding var tabIndex: IndexTabEnum
+    // 所有 tab
+    var tabs: [IndexTabItem]
+    // 监听tab 改变
+    var onTabChange: (_ name: IndexTabEnum) -> Void
+    
+    var body: some View {
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack(alignment: .center, spacing: 10) {
+                ForEach(tabs, id: \.name) { tab in
+                    let isActive = tab.name == tabIndex;
+                    let color: Color = isActive ? .primary : .secondary
+                    Text(tab.title)
+                        .foregroundColor(color)
+                        .font(.subheadline)
+                        .tag(tab.name)
+                        .onTapGesture {
+                            onTapTab(tab: tab)
+                        }
+                }
+            }
+            .padding(
+                EdgeInsets(top: 0, leading: 16, bottom: 0, trailing: 16)
+            )
+        }
+    }
+    
+    /// 点击单个 tab
+    /// - Parameter tab: 当前 tab
+    func onTapTab(tab: IndexTabItem) {
+        self.tabIndex = tab.name
+        self.onTabChange(tab.name)
+    }
+}
+
+//struct HomeNodeTabs_Previews: PreviewProvider {
+//    @State private var tabIndex = IndexTabData[0].name
+//
+//    static var previews: some View {
+//        HomeNodeTabs(tabIndex: $tabIndex)
+//    }
+//}
--- verex/Views/HomeView.swift
+++ verex/Views/HomeView.swift
@@ -8,9 +8,27 @@
 import SwiftUI
 
 struct HomeView: View {
+    @State var tabIndex: IndexTabEnum = IndexTabData[0].name
     var body: some View {
-        Text("Home View")
+        VStack {
+            // 头部导航
+            HomeHead()
+            // 节点标签页
+            HomeNodeTabs(tabIndex: $tabIndex, tabs: IndexTabData, onTabChange: self.onTabChange)
+            // 主内容
+            ScrollView {
+                // Text("Scroll View"')
+            }
+        }        
     }
+    
+    
+    /// 监听节点标签页改变
+    /// - Parameter name: 当前选中标签节点
+    func onTabChange(name: IndexTabEnum) {
+        print("name：\(name.rawValue)")
+    }
+    
 }
 
 struct HomeView_Previews: PreviewProvider {
