fc77a92cba96783c2b241ee54d062b338af99177
CaisanLi <caisanli_0105@163.com>
2022-04-18 22:41:11 +0800

    feat：创建用户Model

---
+++ verex/Model/Member.swift
@@ -0,0 +1,32 @@
+//
+//  Member.swift
+//  verex
+//
+//  Created by 蔡三李 on 2022/4/18.
+//
+
+import Foundation
+
+
+/// 用户信息
+struct Member: Identifiable, Codable {
+    var id: Int
+    var username: String
+    var url: String
+    var avatarMini: URL
+    var avatarNormal: URL
+    var avatarLarge: URL
+    var created: Int
+    var lastModified: Int
+    
+    enum CodingKeys: String, CodingKey {
+        case id
+        case username
+        case url
+        case created
+        case avatarMini = "avatar_mini"
+        case avatarNormal = "avatar_normal"
+        case avatarLarge = "avatar_large"
+        case lastModified = "last_modified"
+    }
+}
