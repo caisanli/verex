5f1e381eae28111d56594648325497d7539bc9fc
CaisanLi <caisanli_0105@163.com>
2022-05-05 22:15:24 +0800

    feat：设置html样式

--- verex/Components/HTMLStringView.swift
+++ verex/Components/HTMLStringView.swift
@@ -17,7 +17,17 @@ struct HTMLStringView: UIViewRepresentable {
     }
 
     func updateUIView(_ uiView: WKWebView, context: Context) {
-        print(htmlContent)
-        uiView.loadHTMLString(htmlContent, baseURL: nil)
+        let newHtml = "<html><head><meta name='viewport' content='width=device-width,initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0'><style>body { font-size: 14px; line-height: 1.6; color: #000; word-break: break-word; margin: 0; padding: 0; } img { max-width: 100%; }</style></head><body>\(htmlContent)</body></html>"
+        uiView.loadHTMLString(newHtml, baseURL: nil)
+        uiView.evaluateJavaScript("document.readyState", completionHandler: { (complete, error) in
+            if complete != nil {
+                uiView.evaluateJavaScript("document.body.scrollHeight", completionHandler: { (result, _) in
+                    if let height = result as? CGFloat {
+                        print(height)
+                    }
+                })
+            }
+
+        })
     }
 }
