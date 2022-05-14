447c06162bbf9428b9eddfff4c85dde2592acae0
CaisanLi <caisanli_0105@163.com>
2022-05-14 10:56:24 +0800

    feat：修改变量命名

--- verex/Utils/api.swift
+++ verex/Utils/api.swift
@@ -12,7 +12,8 @@ import Foundation
 import Alamofire
 
 // 基础地址
-let HOST_URL = "https://www.v2ex.com/api"
+let BASE_URL = "https://www.v2ex.com"
+let BASE_API = "\(BASE_URL)/api"
 
 let BaseHeaders: HTTPHeaders = [
     "Authorization": "Bearer 3de87109-24db-44d6-8375-6b161cce3b58"
@@ -27,23 +28,23 @@ class APIS {
     //   p: 1 // 分页页码 默认为 1
     // }
     // https://www.v2ex.com/api/v2/notifications?p=2
-    static let GET_NOTIFICATIONS = "\(HOST_URL)/v2/notifications"
+    static let GET_NOTIFICATIONS = "\(BASE_API)/v2/notifications"
 
     // 删除指定提醒
     // method: DELETE
     // reset: :notification_id
     // https://www.v2ex.com/api/v2/notifications/123456
-    static let DEL_NOTIFICATION = "\(HOST_URL)/v2/notifications"
+    static let DEL_NOTIFICATION = "\(BASE_API)/v2/notifications"
 
     // 获取自己的Profile
     // method: GET
     // https://www.v2ex.com/api/v2/member
-    static let GET_MEMBER = "\(HOST_URL)/v2/member"
+    static let GET_MEMBER = "\(BASE_API)/v2/member"
 
     // 获取当前使用的令牌
     // method: GET
     // https://www.v2ex.com/api/v2/token
-    static let GET_TOKEN = "\(HOST_URL)/v2/token"
+    static let GET_TOKEN = "\(BASE_API)/v2/token"
 
     // 创建新的令牌
     // method: POST
@@ -52,13 +53,13 @@ class APIS {
     //  expiration: 可支持的值：2592000，5184000，7776000 或者 15552000，即 30 天，60 天，90 天或者 180 天的秒数
     // }
     // https://www.v2ex.com/api/v2/tokens
-    static let CREATE_TOKEN = "\(HOST_URL)/v2/tokens"
+    static let CREATE_TOKEN = "\(BASE_API)/v2/tokens"
 
     // 获取指定节点
     // method: GET
     // reset: :node_name
     // https://www.v2ex.com/api/v2/nodes/python
-    static let GET_NODE = "\(HOST_URL)/v2/nodes"
+    static let GET_NODE = "\(BASE_API)/v2/nodes"
 
     // 获取指定节点下的主题
     // method: GET
@@ -67,13 +68,13 @@ class APIS {
     //  p: 1 分页页码 默认为 1
     // }
     // https://www.v2ex.com/api/v2/nodes/:node_name/topics?p=2
-    static let GET_TOPICES_V2 = "\(HOST_URL)/v2/nodes/${node_name}/topics"
+    static let GET_TOPICES_V2 = "\(BASE_API)/v2/nodes/${node_name}/topics"
 
     // 获取指定主题
     // method: GET
     // reset: :topic_id
     // https://www.v2ex.com/api/v2/topics/1
-    static let GET_TIPC = "\(HOST_URL)/v2/topics"
+    static let GET_TIPC = "\(BASE_API)/v2/topics"
 
     // 获取指定主题下的回复
     // method: GET
@@ -82,7 +83,7 @@ class APIS {
     //  p: 1 分页页码 默认为 1
     // }
     // https://www.v2ex.com/api/v2/topics/1/replies?p=2
-    static let GET_REPLIES = "\(HOST_URL)/v2/topics/${topic_id}/replies"
+    static let GET_REPLIES = "\(BASE_API)/v2/topics/${topic_id}/replies"
     
     // ------ API 1.0
     
@@ -93,7 +94,7 @@ class APIS {
     //  node_name 节点name
     //  username 用户名称
     // }
-    static let GET_TOPICES = "\(HOST_URL)/topics/show.json"
+    static let GET_TOPICES = "\(BASE_API)/topics/show.json"
     
     // 根据主题ID获取回复列表
     // method: GET
@@ -101,7 +102,7 @@ class APIS {
     //  topic_id 主题id
     //  page 当前页
     // }
-    static let GET_REPILES = "\(HOST_URL)/replies/show.json"
+    static let GET_REPILES = "\(BASE_API)/replies/show.json"
 }
 
 struct GET_TOPICES_PARAMS: Codable {
