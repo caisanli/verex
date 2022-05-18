daff73f6d470b46c6015ebd4c0356313632a93b6
CaisanLi <caisanli_0105@163.com>
2022-05-18 22:02:01 +0800

    feat：处理时间格式

--- verex/Components/HowLongAgo.swift
+++ verex/Components/HowLongAgo.swift
@@ -10,14 +10,49 @@ import SwiftUI
 struct HowLongAgo: View {
     let time: Int
     var body: some View {
-        Text("3分钟以前")
+        Text(timeToCurrentTime(time))
             .foregroundColor(.gray)
             .font(.footnote)
+    }
+    
+    /// 将秒转化成多久以前
+    /// - Parameter second: 秒
+    /// - Returns: 多久以前
+    func timeToCurrentTime(_ second: Int) -> String {
+        // 获取当前时间戳
+        let currentTime = Date().timeIntervalSince1970
+        let timeSta: TimeInterval = TimeInterval(time)
+        // 时间差
+        let reduceTime: TimeInterval = currentTime - timeSta
+        // 时间差小于60秒
+        if reduceTime < 60 {
+            return "\(currentTime)秒前"
+        }
+        // 时间差大于一分钟小于60分钟内
+        let mins = Int(reduceTime / 60)
+        if mins < 60 {
+            return "\(mins)分钟前"
+        }
+        // 时间差大于一小时小于24小时内
+        let hours = Int(reduceTime / 3600)
+        if hours < 24 {
+            return "\(hours)小时\(mins % 60)分钟前"
+        }
+        // 时间大于1天且小于一年内
+        let days = Int(reduceTime / (3600 * 24))
+        if days < 365 {
+            return "\(days)天前"
+        }
+        // 都不满足就显示年月日 时分秒
+        let date = NSDate(timeIntervalSince1970: timeSta)
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy年MM月dd日 HH:mm:ss"
+        return formatter.string(from: date as Date);
     }
 }
 
 struct HowLongAgo_Previews: PreviewProvider {
     static var previews: some View {
-        HowLongAgo(time: 3)
+        HowLongAgo(time: 10)
     }
 }
