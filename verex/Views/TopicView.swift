62ae2a90a3fc4330c734df979a949d0b17ee57c3
CaisanLi <caisanli_0105@163.com>
2022-05-19 21:50:56 +0800

    feat：调整布局和回复列表排序

--- verex/Views/TopicView.swift
+++ verex/Views/TopicView.swift
@@ -13,16 +13,34 @@ struct TopicView: View {
     @State var list: [Replies] = []
     
     var body: some View {
-        ScrollView() {
-            // 主题详情
-            TopicInfo(topic: topic)
-            // 主题回复列表
-            ForEach(0..<list.count, id: \.self) { index in
-                let replies = list[index]
-                let isOwner = replies.memberId == topic.member.id
-                TopicReplies(replies: replies, index: index, isOwner: isOwner)
+        VStack {
+            ScrollView() {
+                // 主题详情
+                TopicInfo(topic: topic)
+                // 主题回复列表
+                ForEach(0..<list.count, id: \.self) { index in
+                    let replies = list[index]
+                    let isOwner = replies.memberId == topic.member.id
+                    TopicReplies(replies: replies, index: index, isOwner: isOwner)
+                }
+                .padding(EdgeInsets(top: 0, leading: 16, bottom: 8, trailing: 16))
+                
+                HStack {
+                    Text("没有更多回复")
+                        .font(.system(size: 14))
+                        .foregroundColor(.gray)
+                }
+                .padding(.bottom, 8)
             }
-            .padding(EdgeInsets(top: 0, leading: 16, bottom: 8, trailing: 16))
+            // 底部输入框
+            TopicFooter { reply in
+                onSubmitReply(reply)
+            } onThank: {
+                onThank()
+            } onCollection: {
+                onCollection()
+            }
+
         }
         .onAppear {
             params.page = 1
@@ -31,14 +49,32 @@ struct TopicView: View {
         }
     }
     
+    // 监听感谢
+    func onThank() {
+        print("感谢")
+    }
+    
+    /// 监听收藏
+    func onCollection() {
+        print("收藏")
+    }
+    
+    /// 监听提交回复
+    /// - Parameter reply: 回复内容
+    func onSubmitReply(_ reply: String) {
+        print(reply)
+    }
     
     /// 获取回复列表
     func query() {
         RequestManager.getRelipes(params: params) { result in
+            let newResult = result.sorted { reply1, reply2 in
+                return reply1.created < reply2.created
+            }
             if (params.page == 1) {
-                list = result
+                list = newResult
             } else {
-                list += result
+                list += newResult
             }
         }
     }
