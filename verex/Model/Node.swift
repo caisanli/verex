0496e4d1ba2926e03e6d426ff22e09aebf0c0ba0
CaisanLi <caisanli_0105@163.com>
2022-05-22 21:57:58 +0800

    feat：增加初始值

--- verex/Model/Node.swift
+++ verex/Model/Node.swift
@@ -10,18 +10,21 @@ import Foundation
 
 /// 节点信息
 struct Node: Codable, Hashable, Identifiable {
-    var id: Int
-    var name: String // 唯一name
-    var title: String // 标题
-    var avatar_large: String // 大图
-    var avatar_normal: String // 中图
-    var avatar_mini: String // 小图
+    var id: Int = 0
+    var name: String = "" // 唯一name
+    var title: String = "" // 标题
+    var avatar_large: String = "" // 大图
+    var avatar_normal: String = "" // 中图
+    var avatar_mini: String = "" // 小图
 //    var url: String // 节点主页
-    var topics: Int // 节点下的主题数量
+    var topics: Int = 0 // 节点下的主题数量
 //    var footer: String
-    var header: String // 节点描述
+    var header: String = "" // 节点描述
 //    var stars: Int //
 //    var aliases: [String]
 //    var root: Bool
 //    var parent_node_name: String // 父节点名称
+    
+    init() {}
+    
 }
