ef06548e28a9c6c33a45a6524420a7b8cc79b11e
CaisanLi <caisanli_0105@163.com>
2022-05-24 22:13:44 +0800

    feat：增加详情和列表显示

--- verex/Views/NodeDetailView.swift
+++ verex/Views/NodeDetailView.swift
@@ -10,12 +10,45 @@ import SwiftUI
 struct NodeDetailView: View {
     var nodeName: String;
     @State var info: Node = Node()
+    @State var list: [Topic] = []
     var body: some View {
-        VStack {
+        // 节点一些信息
+        ScrollView {
+            HStack {
+                HStack {
+                    AsyncImage(url: URL(string: info.avatar_large)) { image in
+                        image.resizable()
+                    } placeholder: {
+                        ProgressView()
+                    }
+                    .frame(width: 60, height: 60)
+                    VStack(alignment: .leading) {
+                        HStack {
+                            Text("主题数：\(info.topics)")
+                            Spacer()
+                            Text("加入收藏")
+                                .foregroundColor(.yellow)
+                        }
+                        .font(.system(size: 16))
+                        .padding(.bottom, 8)
+                        
+                        RichText {
+                            info.header
+                        }
+                    }
+                }
+                .padding()
+            }
+            .foregroundColor(.white)
+            .background(.blue)
             
+            ForEach(list) { topic in
+                HomeNodeTopic(topic: topic)
+            }
         }
         .onAppear {
             getInfo()
+            getTopicsList()
         }
     }
     
@@ -24,6 +57,16 @@ struct NodeDetailView: View {
         let params = GET_NODE_INFO_PARAMS(name: nodeName)
         RequestManager.getNodeInfo(params: params) { info in
             self.info = info
+            print(info)
+        }
+    }
+    
+    
+    /// 获取主题列表
+    func getTopicsList() {
+        let params = GET_TOPICES_PARAMS(node_name: nodeName)
+        RequestManager.queryTopics(params: params) { result in
+            self.list = result
         }
     }
 }
