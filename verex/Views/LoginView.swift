d346572e3db3722d65436aa2646201704de1566f
CaisanLi <caisanli_0105@163.com>
2022-06-13 22:23:52 +0800

    feat(LoginView)：添加登录表单

--- verex/Views/LoginView.swift
+++ verex/Views/LoginView.swift
@@ -8,8 +8,60 @@
 import SwiftUI
 
 struct LoginView: View {
+    @State private var username: String = ""
+    @State private var password: String = ""
+    @State private var code: String = ""
+    @State private var once: String = ""
+    @State private var codeImage: String = "https://www.v2ex.com/_captcha"
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            
+            Form {
+                Text("verex")
+                    .font(.largeTitle)
+                    .padding(.bottom)
+                    .padding(.top, 8)
+                TextField("用户名/邮箱", text: $username)
+                SecureField("密码", text: $password)
+                HStack {
+                    TextField("验证码", text: $code)
+                    AsyncImage(url: URL(string: codeImage)) { image in
+                        image.resizable()
+                    } placeholder: {
+                        ProgressView()
+                    }
+                    .frame(width: 120, height: 36, alignment: .center)
+
+                }
+                
+                HStack {
+                    Spacer()
+                    Button("登录") {
+                        print("登录")
+                    }
+                    .buttonStyle(.borderedProminent)
+                    Spacer()
+                }
+                .padding(.vertical, 8)
+                
+            }
+            .font(.system(size: 14))
+            
+        }
+        .navigationTitle("登录")
+        .navigationBarTitleDisplayMode(.inline)
+        .onAppear {
+            getLoginParamsKey()
+        }
+    }
+    
+    func getLoginParamsKey() {
+        RequestManager.getLoginHtml { html in
+            let paramsKey = LOGIN_PARAMS_KEY(from: html)
+//            paramsKey.code
+            
+        }
     }
 }
 
