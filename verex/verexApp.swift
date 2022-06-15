33545e4bf95277075cda850191a4a201eb5baa02
CaisanLi <caisanli_0105@163.com>
2022-06-15 22:49:35 +0800

    feat(*)：增加全局登录状态维护

--- verex/verexApp.swift
+++ verex/verexApp.swift
@@ -9,9 +9,11 @@ import SwiftUI
 
 @main
 struct verexApp: App {
+    var login: LoginVM = LoginVM()
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(login)
         }
     }
 }
