1f9665b1b65af41158079663fb7c49bfd8fac73c
CaisanLi <caisanli_0105@163.com>
2022-06-15 22:50:12 +0800

    feat(LoginVM)：增加初始化时获取登录用户信息

--- verex/ViewModel/LoginVM.swift
+++ verex/ViewModel/LoginVM.swift
@@ -10,9 +10,22 @@ import Foundation
 class LoginVM: ObservableObject {
     @Published var isLogin: Bool = false
     @Published var user: Member? = nil
-//    @Published var loginForm: LoginForm = LoginForm()
     
-    func login() {
+    init() {
+        self.getMember()
+    }
+    
+    func getMember() {
+        let username = UserDefaultKeys.string(.loginUser) ?? ""
         
+        guard !username.isEmpty else {
+            self.isLogin = false
+            return
+        }
+        
+        let params = GET_MEMBER_PARAMS(username: username)
+        RequestManager.getMember(params: params) { member in
+            self.user = member
+        }
     }
 }
